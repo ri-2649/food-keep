8f1fbcc7aee6354200a8a0b006943c4135ee1782
ri-2649 <rinka82649@gmail.com>
2026-07-09 16:35:09 +0900

    新規作成

---
+++ files/sw.js
@@ -0,0 +1,42 @@
+// 保存帖 - Service Worker（オフライン対応）
+const CACHE_NAME = 'hozoncho-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.js',
+  './manifest.json',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        // フォントなど外部リソースはキャッシュせずそのまま返す
+        if (event.request.url.startsWith(self.location.origin)) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
